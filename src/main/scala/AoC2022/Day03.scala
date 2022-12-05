6f93c4b90f3b3d4f32d1b7fbb5a099272994e9e3
Ash <maybork@pm.me>
2022-12-05 20:11:00 +0100

    add day 3

---
+++ src/main/scala/AoC2022/Day03.scala
@@ -0,0 +1,34 @@
+package AoC2022
+
+import scala.compiletime.ops.int
+
+object Day03 extends App:
+  def evalRucksack(line: String): Int =
+    val (a, b) = line.splitAt(line.length() / 2);
+    val matching = a.filter(b.contains(_)).charAt(0)
+    priority(matching)
+
+  def evalThreeRucksacks(sacks: Seq[String]): Int =
+    val (a, b, c): (String, String, String) = (sacks(0), sacks(1), sacks(2));
+    val matching: Char = a.filter(b.contains(_)).filter(c.contains(_)).charAt(0)
+    priority(matching)
+
+  def priority(item: Char): Int =
+    item.isUpper match
+      case true  => item.charValue() - 38
+      case false => item.charValue() - 96
+
+  def part1(data: Seq[String]): Int =
+    data.map(evalRucksack).sum
+
+  def part2(data: Seq[String]): Int =
+    var dataIter = data.iterator
+    var result: Int = 0
+    while dataIter.hasNext do
+      result += evalThreeRucksacks(dataIter.take(3).toSeq);
+    result
+  def main() =
+    val input = io.Source.fromResource("AoC2022/day03.txt").getLines().toSeq
+    println(s"A ${part1(input)}")
+    println(s"B ${part2(input)}")
+  main()
